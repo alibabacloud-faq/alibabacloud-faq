9355606ba8c3d01c44a3b3569fcefe43b8d2581e
mosuke5 <w.vamos603+github@gmail.com>
2018-07-22 19:13:41 +0900

    add ci/cd setting (#1)
    * add ci setting

---
+++ scripts/hugo-entry-to-elasticsearch-json/main.rb
@@ -0,0 +1,41 @@
+require "pp"
+require "json"
+require "tomlrb"
+require "commonmarker"
+require "sanitize"
+
+# path of hugo markdown contents
+contents_path = ARGV[0]
+
+Dir.glob("#{contents_path}**/*.md") do |entry_list|
+  uri = entry_list.slice(contents_path.length..-4)
+  File.open(entry_list, "r") do |entry| 
+    # Split data to meta info and body
+    entry_raw_data = entry.read.split("+++\n")
+
+    # Convert meta info(toml) to ruby obj
+    entry_data = Tomlrb.parse(entry_raw_data[1])
+
+    # temporary hash for output
+    tmp = {}
+    
+    # convert hash key name to letter(ex. Tags -> tags)
+    entry_data.each do |k, v|
+      tmp[k.downcase] = v
+    end
+
+    
+    # sanitize html tags
+    body_html = CommonMarker.render_html(entry_raw_data[2])
+    body = Sanitize.fragment(body_html)
+    tmp["body"] =  body
+    tmp["body_length"] = body.length
+    tmp["uri"] = "/faq/#{uri}"
+
+    # output 
+    if entry_data["draft"] == false
+      puts "{ \"index\" : { \"_index\" : \"faq\", \"_type\" : \"faq\", \"_id\" : \"#{uri}\" } }"
+      puts tmp.to_json
+    end
+  end
+end
